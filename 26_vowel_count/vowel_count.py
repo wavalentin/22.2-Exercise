968ce99731c99b999c85aa41288ca8151b4d66ad
Willie <willieavalentin@gmail.com>
2024-05-05 18:40:14 -0400

    adding files

---
+++ 26_vowel_count/vowel_count.py
@@ -0,0 +1,16 @@
+def vowel_count(phrase):
+    """Return frequency map of vowels, case-insensitive.
+
+        >>> vowel_count('rithm school')
+        {'i': 1, 'o': 2}
+        
+        >>> vowel_count('HOW ARE YOU? i am great!') 
+        {'o': 2, 'a': 3, 'e': 2, 'u': 1, 'i': 1}
+    """
+    counter = {}
+
+    for ltr in phrase:
+        if ltr in VOWELS:
+            counter[ltr] = counter.get(ltr, 0) + 1
+
+    return counter
